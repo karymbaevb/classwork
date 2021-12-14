1cf5978fb27d47ee7a711ab2d33213c111cdbf41
bkarymbaev <bkarymbaev@gmail.com>
2021-12-15 00:52:41 +0600

    practice

--- src/kg/itacademy/fv21/modfinal/Main.java
+++ src/kg/itacademy/fv21/modfinal/Main.java
@@ -5,11 +5,10 @@ import javax.swing.*;
 public class Main {
 
     public static void main(final String[] args) {
-        Drawer drawer = new Drawer();
         JFrame jFrame = new JFrame();
         jFrame.setSize(500, 500);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.getContentPane().add(drawer);
+//        jFrame.getContentPane().add(drawer);
         jFrame.setVisible(true);
     }
 
